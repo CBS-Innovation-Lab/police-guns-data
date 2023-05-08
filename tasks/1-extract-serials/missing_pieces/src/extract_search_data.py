948c40bc396521477141715ebcd8bfca65453d54
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-05-08 22:53:10 +0000

    update missing pieces descriptions

--- tasks/1-extract-serials/missing_pieces/src/extract_search_data.py
+++ tasks/1-extract-serials/missing_pieces/src/extract_search_data.py
@@ -3,12 +3,25 @@
 import sys
 import pandas as pd
 
+
+def get_description(row):
+    """generates a description based on row data"""
+    desc = ""
+    if pd.notna(row["STATUS"]):
+        desc += f'[{row["STATUS"]}]'
+    if pd.notna(row["OFFENSE"]):
+        desc += f' [{row["OFFENSE"]}]'
+    if pd.notna(row["MAKE"]):
+        desc += f" {row['MAKE']}"
+    if pd.notna(row["MODEL"]):
+        desc += f" {row['MODEL']}"
+    if pd.notna(row["PROPERTY_DESCRIPTION"]):
+        desc += f" {row['PROPERTY_DESCRIPTION']}"
+    return desc
+
+
 df = pd.read_csv(sys.argv[1])
-df = df.assign(
-    description=lambda df: df[["MAKE", "MODEL", "PROPERTY_DESCRIPTION"]].apply(
-        lambda row: " ".join([str(x) for x in row]), axis=1
-    ),
-)
+df["description"] = df.apply(get_description, axis=1)
 df[["SERIAL NUMBER", "DATE", "AGENCY", "description"]].to_csv(
     sys.argv[2], index=False, header=False
 )
