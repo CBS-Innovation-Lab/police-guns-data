2d95af229d6c91ef1700c326955d32c13f8ca3a1
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-05-09 17:00:06 +0000

    update function

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,20 @@
 
 import os
 from airtable import Airtable
+from dotenv import load_dotenv
+
+DOTENV = load_dotenv(dotenv_path=".env")
 
 BASE_ID = "appo35FnVrocw6SO5"
 
 
 def get_airtable_client():
     """gets an airtable client"""
-    return Airtable(BASE_ID, os.getenv("AIRTABLE_API_KEY"))
+    api_key = os.environ.get("AIRTABLE_API_KEY")
+    if not api_key:
+        msg = "AIRTABLE_API_KEY environment variable not set"
+        if not DOTENV:
+            msg += ". Did you forget to put a .env file in your task?"
+        raise ValueError(msg)
+
+    return Airtable(BASE_ID, api_key)
