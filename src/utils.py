67d3d70e8f9da17b996db27d9050bec5e87e8655
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-06-02 20:44:17 +0000

    move load_sale_data to utils

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@
 import os
 from airtable import Airtable
 from dotenv import load_dotenv
+import pandas as pd
 
 DOTENV = load_dotenv(dotenv_path=".env")
 
@@ -19,3 +20,52 @@ def get_airtable_client():
         raise ValueError(msg)
 
     return Airtable(BASE_ID, api_key)
+
+
+def load_sale_data():
+    """loads the sale data from airtable"""
+    client = get_airtable_client()
+    response = client.get("Serials")
+    if len(response["records"]) == 0:
+        raise ValueError("No records found in airtable")
+
+    rows = []
+    offset = None
+    while True:
+        response = client.get("Serials", offset=offset)
+        rows.extend(response["records"])
+        if "offset" in response:
+            offset = response["offset"]
+        else:
+            break
+
+    get_key_if_exists = (
+        lambda x, y: x[y]  # pylint: disable=unnecessary-lambda-assignment
+        if y in x
+        else None
+    )
+    df = (
+        pd.DataFrame(rows)
+        .assign(
+            serial=lambda df: df.fields.apply(
+                lambda x: get_key_if_exists(x, "Serial number")
+            ),
+            sale_date=lambda df: df.fields.apply(
+                lambda x: pd.to_datetime(get_key_if_exists(x, "Sale date"))
+            ),
+            sale_description=lambda df: df.fields.apply(
+                lambda x: get_key_if_exists(x, "Firearm description")
+            ),
+            sale_agency=lambda df: df.fields.apply(
+                lambda x: get_key_if_exists(x, "agency_name")
+            ),
+        )
+        .drop(
+            [
+                "createdTime",
+                "fields",
+            ],
+            axis=1,
+        )
+    )
+    return df
--- tasks/5-do-search/src/do_search.py
+++ tasks/5-do-search/src/do_search.py
@@ -6,58 +6,9 @@ import sqlite3
 import sys
 import pandas as pd
 from tqdm import tqdm
-from src.utils import get_airtable_client
+from src.utils import load_sale_data
 
 logging.basicConfig(level=logging.INFO, filename="output/do_search.log", filemode="w")
-
-
-def load_sale_data():
-    """loads the sale data from airtable"""
-    client = get_airtable_client()
-    response = client.get("Serials")
-    if len(response["records"]) == 0:
-        raise ValueError("No records found in airtable")
-
-    rows = []
-    offset = None
-    while True:
-        response = client.get("Serials", offset=offset)
-        rows.extend(response["records"])
-        if "offset" in response:
-            offset = response["offset"]
-        else:
-            break
-
-    get_key_if_exists = (
-        lambda x, y: x[y]  # pylint: disable=unnecessary-lambda-assignment
-        if y in x
-        else None
-    )
-    df = (
-        pd.DataFrame(rows)
-        .assign(
-            serial=lambda df: df.fields.apply(
-                lambda x: get_key_if_exists(x, "Serial number")
-            ),
-            sale_date=lambda df: df.fields.apply(
-                lambda x: pd.to_datetime(get_key_if_exists(x, "Sale date"))
-            ),
-            sale_description=lambda df: df.fields.apply(
-                lambda x: get_key_if_exists(x, "Firearm description")
-            ),
-            sale_agency=lambda df: df.fields.apply(
-                lambda x: get_key_if_exists(x, "agency_name")
-            ),
-        )
-        .drop(
-            [
-                "createdTime",
-                "fields",
-            ],
-            axis=1,
-        )
-    )
-    return df
 
 
 def preprocess_serials(df):
